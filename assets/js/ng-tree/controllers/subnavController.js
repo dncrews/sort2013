b8baad66e12df03c27d9839ba20b01694a274b26
Mark Gardner <gardnerm@familysearch.org>
2013-09-24 08:49:21 -0600

    Adding active flag to subnav

--- assets/js/ng-tree/controllers/subnavController.js
+++ assets/js/ng-tree/controllers/subnavController.js
@@ -1,17 +1,25 @@
 treeApp.controller('subnavController', ['$scope', function($scope) {
 	$scope.pages = [{
+		name: 'tree',
 		text: lang.nav_tree,
 		icon: 'fs-icon-tree',
 		hasHistory: true
 	}, {
+		name: 'ancestor',
 		text: lang.nav_ancestor,
 		icon: 'fs-icon-ancestor',
 		hasHistory: true
 	}, {
+		name: 'search',
 		text: lang.nav_search,
 		icon: 'fs-icon-search'
 	}, {
+		name: 'watch',
 		text: lang.nav_watch,
 		icon: 'fs-icon-tree'
 	}];
+
+	$scope.isActive = function(page) {
+		return this.pagename === page.name;
+	}
 }]);
