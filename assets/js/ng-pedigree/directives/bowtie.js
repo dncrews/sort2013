9c10c172ee2a205be08ce6c3f430e1cf82e7b67e
Mark Gardner <gardnerm@familysearch.org>
2013-10-02 22:30:10 -0600

    Adding watch for pedigree.

--- assets/js/ng-pedigree/directives/bowtie.js
+++ assets/js/ng-pedigree/directives/bowtie.js
@@ -6,7 +6,8 @@ pedigreeApp.directive('fsBowtie', ['pedigreeService', function(pedigreeService) 
 			root: '=',
 			x: '@',
 			y: '@',
-			showprimary: '@'
+			showprimary: '@',
+			person: '='
 		},
 		template: 
 '<div class="bowtie-container">\
@@ -46,6 +47,27 @@ pedigreeApp.directive('fsBowtie', ['pedigreeService', function(pedigreeService) 
 					return link;
 				}
 			}
+
+			scope.$watch('person.nameConclusion.details.fullText', function(newVal, oldVal, scope) {
+				function updateName(couple, id, name) {
+					if(!couple) {
+						return;
+					}
+
+					if(id === (couple.husband && couple.husband.id)) {
+						couple.husband.name = name;
+					} else if(id === (couple.wife && couple.wife.id)) {
+						couple.wife.name = name;
+					}
+
+					updateName(couple.husband && couple.husband.parents, id, name);
+					updateName(couple.wife && couple.wife.parents, id, name);
+				}
+
+				if(newVal !== oldVal) {
+					updateName(scope.bowtie, scope.person && scope.person.id, newVal);
+				}
+			});
 		}
 	}
 }]);
--- assets/js/ng-pedigree/services/pedigreeService.js
+++ assets/js/ng-pedigree/services/pedigreeService.js
@@ -2,7 +2,6 @@ pedigreeApp.factory('pedigreeService', ['$http', function($http) {
 	return {
 		getData: function(cb) {
 			$http.get('/data-pedigree').success(function(data) {
-				console.log(data);
 				cb(data);
 			});
 		}
