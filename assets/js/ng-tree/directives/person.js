2ffd8532243053aecf89d5da19a8a0f16cdc3adf
Mark Gardner <gardnerm@familysearch.org>
2013-10-03 13:26:13 -0600

    Adding support for empty lifespans on persons.

--- assets/js/ng-tree/directives/person.js
+++ assets/js/ng-tree/directives/person.js
@@ -10,6 +10,15 @@ treeApp.directive('fsPerson', function() {
 		templateUrl: 'partials/person',
 		link: function(scope, element, attrs) {
 			// Add person card trigger
+			scope.calcLabel = function(person) {
+				if(person.lifeSpan && person.id) {
+					return person.lifeSpan + ' • ' + person.id;
+				} else if(person.lifeSpan) {
+					return person.lifeSpan;
+				} else if(person.id) {
+					return person.id;
+				}
+			}
 		}
 	}
 });
